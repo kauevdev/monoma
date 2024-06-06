2c9fafbb56463f8e87114ee1f2996caf4fba618a
Kauê Vieira <skaues248@gmail.com>
2024-06-05 21:44:05 -0300

    novas frases adicionadas

--- Monoma.js
+++ Monoma.js
@@ -2,7 +2,7 @@ const chatBox = document.getElementById('chat-box');
 const userInput = document.getElementById('user-input');
 
 function sendMessage() {
-  const userMessage = userInput.value;
+  const userMessage = userInput.value.toLowerCase(); // Converter para minúsculas
   appendMessage('user', userMessage);
   setTimeout(() => {
     const botMessage = getBotResponse(userMessage);
@@ -47,36 +47,140 @@ function calcularOperacao(operacao, num1, num2) {
   return resultado;
 }
 
-const responses = {
-  'quero ver sua logo': '<img src="monoma.png" alt="Logo da Monoma">',
-  'me envie sua logo': '<img src="monoma.png" alt="Logo da Monoma">',
-  'foto de um gato': '<img src="https://www.pngall.com/wp-content/uploads/2016/05/Kitten.png" alt="gatinho fofo">',
-  'video random': '<iframe width="300" height="300" src="https://www.youtube.com/embed/ba7--Hp7EQU" title="BEAT ZÉ DA MANGA 🥭 - Aaah zé da manga (FUNK REMIX) by Sr. Nescau" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
-  'video phonk': '<iframe width="300" height="300" src="https://www.youtube.com/embed/dvQJIgjlR3I" title="MoonDeity - NEON BLADE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
-  'seu nome': 'Sei lá... pergunta para Carlos.',
-  'perguntas': 'Aqui estão algumas sugestões de perguntas: \n1.Por que o seu nome é Monoma? \n2.Como você está? \n3.Quais são algumas curiosidades sobre você? \n4.A Monoma tem alguma habilidade especial? \n5.Qual é a função da Monoma? \n6.O que você pode fazer? \n7.Como surgiu sua logo?\n8.Faça um cálculo matemático (por exemplo: "Calcule 2 mais 2") \n9.Pode me enviar uma foto? \n10.Pode me enviar um video? \n Lembre-se que você deve digitar dessa mesma maneira, pois sou uma IA exata 🧐.',
-  'como você está': 'Estou bem, obrigada por perguntar! 😃',
-  'enviar uma foto': 'Claro, tente "Me envie sua logo" ou "Me envie uma foto de um gato". Lembre-se que você deve digitar dessa mesma maneira, pois sou uma IA exata 🧐',
-  'enviar um video': 'Sim! Tente "Me envie um video phonk" ou "Me envie um video random". Lembre-se que você deve digitar dessa mesma maneira, pois sou uma IA exata 🧐',
+// Objetos para diferentes tipos de respostas
+const greetings = {
   'oi': 'Olá! Como posso ajudar?',
   'olá': 'Olá! Como posso ajudar?',
-  'tudo bem': 'Sim, estou bem! :)',
-  'você é humano': 'Não, sou uma assistente virtual criada para um trabalho sobre IA.',
-  'você é uma pessoa': 'Não, sou uma assistente virtual criada para um trabalho sobre IA.',
+  'ola': 'Olá! Como posso ajudar?',
+  'oie': 'Oi! Em que posso te ajudar?',
+  'eai': 'E aí! Como posso ser útil?',
+  'eae': 'E aê! Em que posso te ajudar?',
+  'opa': 'Olá! Estou aqui para te ajudar!',
+  'aoba': 'Aoba! Como posso ser útil?',
+  'bom dia': 'Bom dia! Como posso te ajudar?',
+  'boa tarde': 'Boa tarde! Em que posso te auxiliar?',
+  'boa noite': 'Boa noite! Precisa de alguma ajuda?',
+  'como você está': 'Estou bem, obrigada por perguntar! 😊',
+  'tudo bem': 'Sim, estou bem! 😃',
+  'está tudo bem': 'Sim, estou bem! 😊',
+  'tudo certo': 'Sim, estou bem! 😃',
+  'como tem passado': 'Estou indo bem, obrigada por perguntar! 😊',
+  'como vai': 'Estou indo bem, obrigada por perguntar! 😊',
+  'td bom': 'Tudo ótimo! 😊',
+  'tudo bão': 'Tudo ótimo! 😊',
+  'td bão': 'Tudo ótimo! 😊'
+};
+
+const botInfo = {
+  'seu nome': 'Sei lá... pergunta para Carlos.',
   'sobre você': 'Algumas curiosidades sobre mim são: a Monoma é uma assistente virtual criada por Kauê Vieira, nomeada por Carlos Henrique Borges e sua logo foi Feita Por Daline Soares, A Monoma foi criada para um projeto acadêmico sobre inteligência artificial.',
   'habilidade especial': 'Como uma assistente virtual, minha habilidade especial é ajudar os usuários com suas perguntas e tarefas, posso enviar imagens, vídeos e resolver contas envolvendo as operações básicas.',
   'função': 'A função da Monoma é servir como uma assistente virtual para facilitar a vida dos usuários, fornecendo suporte, informações e executando tarefas de forma rápida e eficiente. E também divertir o usuário.',
-  'o que você pode fazer': 'Na teoria eu poderia fornecer informações sobre diversos tópicos, responder perguntas e auxiliar em tarefas simples. No entanto, Kauê ficou com preguiça de criar mais códigos para me ensinar mais coisas 🥲. No momento posso resolver contas básicas de matemática, enviar imagens e videos.',
+  'o que você pode fazer': 'Na teoria eu poderia fornecer informações sobre diversos tópicos, responder perguntas e auxiliar em tarefas simples. No entanto, Kauê ficou com preguiça de criar mais códigos para me ensinar mais coisas 🥲. No momento posso resolver contas básicas de matemática, enviar imagens e vídeos.',
   'surgiu sua logo': 'Foi Daline que fez, Diga a ela que ficou bem legal! 😉',
 };
 
+const facts = [
+  "A Antártida é o maior deserto do mundo.",
+  "A lua cheia é mais brilhante do que a lua em qualquer outra fase.",
+  "O DNA humano é aproximadamente 99,9% idêntico entre todas as pessoas.",
+  "Os ursos polares são canhotos.",
+  "O sol é cerca de 330.000 vezes mais massivo do que a Terra.",
+  "O Japão flutua sobre sete placas tectônicas.",
+  "Os elefantes são os únicos mamíferos que não podem pular.",
+  "O azul é a cor mais quente da chama de uma vela.",
+  "O Burj Khalifa, em Dubai, é o edifício mais alto do mundo.",
+  "Os coelhos e os papagaios podem ver atrás de si sem mover a cabeça.",
+  "Um mol é aproximadamente igual ao número de átomos de carbono em 12 gramas de carbono-12.",
+  "Os golfinhos têm nomes próprios.",
+  "A Grande Muralha da China não é visível da Lua a olho nu.",
+  "Os ossos humanos são mais fortes do que o concreto.",
+  "O olho humano é capaz de distinguir entre aproximadamente 10 milhões de cores.",
+  "Orelhas e narizes nunca param de crescer.",
+  "Os furacões raramente atingem a linha do equador.",
+  "Os camelos têm três pálpebras para protegerem os olhos de tempestades de areia.",
+  "Um único raio de um relâmpago é cinco vezes mais quente do que a superfície do sol.",
+  "A Ilha de Páscoa é a localização mais remota de qualquer civilização do mundo."
+];
+
+
+
+const filosofia = [
+  "Penso, logo existo. - René Descartes",
+  "A vida é sem sentido sem a arte. - Platão",
+  "A maior sabedoria é perceber a própria ignorância. - Sócrates",
+  "O homem nasce livre, mas está aprisionado em todos os lugares. - Jean-Jacques Rousseau",
+  "O conhecimento é poder. - Francis Bacon",
+  "Só sei que nada sei. - Sócrates",
+  "A vida é curta, mas a arte é longa. - Hipócrates",
+  "O homem é a medida de todas as coisas. - Protágoras",
+  "O que não nos mata nos torna mais fortes. - Friedrich Nietzsche",
+  "Quem luta com monstros deve acautelar-se para não se tornar também um monstro. Quando se olha muito tempo para um abismo, o abismo olha para você. - Friedrich Nietzsche",
+];
+
+
+const iframes = {
+  'iframe': '<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+};
+
+const images = {
+  'imagem': '<img src="imagem.png" alt="Descrição da imagem">',
+};
+
+const customResponses = {
+  'resposta_personalizada': 'Aqui está uma resposta personalizada para você!',
+};
+
+
+
 function getBotResponse(userMessage) {
-  const keys = Object.keys(responses);
+  // Converter a mensagem do usuário para minúsculas
   const message = userMessage.toLowerCase();
 
-  for (const key of keys) {
-    if (message.includes(key)) {
-      return responses[key];
+  // Verificar saudações
+  for (const greeting in greetings) {
+    if (message.includes(greeting)) {
+      return greetings[greeting];
+    }
+  }
+
+  // Verificar informações sobre o bot
+  for (const info in botInfo) {
+    if (message.includes(info)) {
+      return botInfo[info];
+    }
+  }
+
+  // Verificar se o usuário solicita um fato aleatório
+  if (message.includes('fato')) {
+    const randomFactIndex = Math.floor(Math.random() * facts.length);
+    return facts[randomFactIndex];
+  }
+
+  // Verificar se o usuário solicita uma frase filosofica aleatória
+  if (message.includes('filosofica')) {
+    const randomFilosofiaIndex = Math.floor(Math.random() * filosofia.length);
+    return filosofia[randomFilosofiaIndex];
+  }
+
+  // Verificar iframes
+  for (const iframe in iframes) {
+    if (message.includes(iframe)) {
+      return iframes[iframe];
+    }
+  }
+
+  // Verificar imagens
+  for (const image in images) {
+    if (message.includes(image)) {
+      return images[image];
+    }
+  }
+
+  // Verificar outras respostas personalizadas
+  for (const response in customResponses) {
+    if (message.includes(response)) {
+      return customResponses[response];
     }
   }
 
@@ -93,8 +197,7 @@ function getBotResponse(userMessage) {
     if (operacaoEncontrada) {
       const numeros = message.match(/\d+/g);
       if (numeros && numeros.length >= 2) {
-        const num1 = parseFloat(numeros[0
-]);
+        const num1 = parseFloat(numeros[0]);
         const num2 = parseFloat(numeros[1]);
         const resultado = calcularOperacao(operacaoEncontrada, num1, num2);
         return `${resultado}. Achei fácil 😎`;
@@ -108,6 +211,7 @@ function getBotResponse(userMessage) {
     return 'Desculpe, não entendi.\n\nVocê gostaria de fazer alguma pergunta? Se sim, digite: "Perguntas".';
   }
 }
+
 
 function appendMessage(sender, message) {
   const messageElement = document.createElement('div');
